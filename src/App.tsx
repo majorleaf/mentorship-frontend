c9965242743582774df18f9fb75efa65d518acde
Madandola james <jamesmadandola@gmail.com>
2025-08-12 14:40:42 +0100

    changed files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+    <div className='bg-black text-white' >
+      welcome to our frontend project
+    </div>
+    </>
+  )
+}
+
+export default App
