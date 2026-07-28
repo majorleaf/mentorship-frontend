3a750fe08ef93c327f89d02d42c5728853f3249f
Madandola james <jamesmadandola@gmail.com>
2026-07-28 06:16:38 +0100

    refactor

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black text-white">   welcome to frontend project </div>
+    </>
+  )
+}
+
+export default App
