05a6fe30e4a3705fb3b2c0d07834e95f344d0fa9
Madandola james <jamesmadandola@gmail.com>
2026-07-30 17:44:46 +0100

    updated

--- my-app/src/auth/login.tsx
+++ my-app/src/auth/login.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
 export default function Login() {
+        
   return (
     <div className='mt-10 justify-self-center mx-auto' >
       <h1 className='justify-self-center justify-center font-bold text-2-3x1'>Login to Guidewire</h1>
-      <form className='max-w-md mx-auto mt-10 p-8 bg-white rounded-x1 shadow-1g '> 
+      <form className='max-w-md mx-auto mt-10 p-8 bg-white rounded-x1 shadow-1g ' onSubmit={Submit}> 
         <h1 className='text-3xl font-bold text-center mb-4'> Login</h1>
            <div className='mb-4'>
             <label htmlFor='email' className='black text-black-600 mb-78'>
-              Email address
+              Email 
             </label>
-            <input type='email' placeholder='type in valid email addess' required className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'>
+            <input type='email'
+             id='email'
+              placeholder='type in valid email addess'
+               required
+                className='w-full rounded-x1 px-4 py-2 pl=11 pr-4 text-sm text0slate-900 placeholder:text-slate-400 focus:border-blue-600   border  border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'>
             </input>
            </div>
            <div className='mb-4'>
@@ -18,9 +24,10 @@ export default function Login() {
             <label htmlFor='email' className='black text-black-600 mb-2'>
               Password
             </label>
-            <input type='password' placeholder=' password' required className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'>
+            <input type='password' placeholder='password' required className='w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'>
             </input>
-            <button className=' absolute right-3 top-9  focus: outline-none rounded-1g shadow-amber-200s border mt-1' >
+            <button type='submit' 
+            className=' flex w-full items-center justify-center gap-2 rounded-x1 bg=slate-900 py-3 text-sm font-semibold transition-colors hover:bg-slate-800 ' >
                   Login
             </button>
            </div>
