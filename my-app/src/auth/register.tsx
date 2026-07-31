acad6a4dca30affc0773189ccd0dc7a8f25c520c
Madandola james <jamesmadandola@gmail.com>
2026-07-31 04:34:06 +0100

    updated

--- my-app/src/auth/register.tsx
+++ my-app/src/auth/register.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>R</div>
+    <div className='flex min-h-screen font-sans bg-white'>
+      
+      <div className=''>
+        <div className=''>
+
+          {/* Logo */}
+          <div className='mb-10  flex items-center gap-2'>
+              <div className=' flex h-8  items-center justify-center rounded bg-blue-600 shodow-sm'>
+                <span className='text-x1 font-bold leading-none text-white'>G</span>
+              </div>
+          </div>
+
+
+        </div>
+      </div>
+      R</div>
   )
 }
