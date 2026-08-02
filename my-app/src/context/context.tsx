3e4b672a09de980c22fb7fcc7c0d545a1fa31d9a
Madandola james <jamesmadandola@gmail.com>
2026-08-02 06:53:55 +0100

    fix: flatten nested my-app folder

--- my-app/src/context/context.tsx
+++ my-app/src/context/context.tsx
@@ -11,12 +11,15 @@ interface ShopContextProviderProps {
     children: ReactNode;
 }
 
-export const ShopContextProvider: React.FC<ShopContextProviderProps> = ({ children }) => {
+export const ShopContextProvider: React.FC<ShopContextProviderProps> = (props) => {
     const backendUrl = "https://mentorship-backend-r9vb.onrender.com";
 
+    const value: ShopContextType = {
+        backendUrl,
+    };
     return (
-        <ShopContext.Provider value={{ backendUrl }}>
-            {children}
+        <ShopContext.Provider value={value}>
+            {props.children}
         </ShopContext.Provider>
     );
 };
